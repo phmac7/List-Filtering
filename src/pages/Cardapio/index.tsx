760a7a4ad2a5292db66908c71dbe7c4581a2b313
Pedro Machado <phvieiras@hotmail.com>
2022-03-31 03:53:18 -0300

    prox: criar itens

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,35 @@
+import styles from './Cardapio.module.scss'
+import { ReactComponent as Logo } from 'assets/logo.svg'
+import Buscador from './Buscador'
+import { useState } from 'react'
+import Filtros from './Filtros'
+import Ordenador from './Ordenador'
+
+function Cardapio() {
+    const [busca, setBusca] = useState("")
+    const [filtro, setFiltro] = useState<number | null>(null)
+    const [ordenador, setOrdenador] = useState('')
+
+    return (
+        <main>
+            <nav className={styles.menu}>
+                <Logo />
+            </nav>
+            <header className={styles.header}>
+                <div className={styles.header__text}>
+                    <span>A casa do código e da massa</span>
+                </div>
+            </header>
+            <section className={styles.cardapio}>
+                <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+                <Buscador busca={busca} setBusca={setBusca} />
+                <div className={styles.cardapio__filtros}>
+                    <Filtros filtro={filtro} setFiltro={setFiltro} />
+                    <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
+                </div>
+            </section>
+        </main>
+    )
+}
+
+export default Cardapio
